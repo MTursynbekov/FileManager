ecee63aa1f94f9d55ca72190c30363e8bee15c5f
Crazy Cookie <mr.tmk_0210@mail.ru>
2021-03-13 19:11:11 +0600

    Finished

--- FileManager/Layer.cs
+++ FileManager/Layer.cs
@@ -1,10 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace FileManager
 {
     class Layer
     {
+        public FileSystemInfo[] Content
+        {
+            get;
+            set;
+        }
+
+        public int SelectedItem
+        {
+            get;
+            set;
+        }
+
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            for (int i = 0; i < Content.Length; ++i)
+            {
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Write(Content[i].Name);
+
+                    if (Content[i].GetType() == typeof(FileInfo))
+                    {
+                        FileInfo file = (FileInfo)Content[i];
+                        Console.WriteLine("             " + file.Length + " Bytes");
+                    }
+                    else
+                    {
+                        DirectoryInfo dir = (DirectoryInfo)Content[i];
+                        Console.WriteLine("             " + DirSize(dir) + " Bytes");
+                    }
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine(Content[i].Name);
+                }
+            }
+        }
+
+        public void ScrollUp()
+        {
+            if (SelectedItem <= 0)
+            {
+                SelectedItem = Content.Length - 1;
+            }
+            else
+            {
+                SelectedItem--;
+            }
+        }
+
+        public void ScrollDown()
+        {
+            if (SelectedItem >= Content.Length -1)
+            {
+                SelectedItem = 0;
+            }
+            else
+            {
+                SelectedItem++;
+            }
+        }
+
+        public void GoBegin()
+        {
+            SelectedItem = 0;
+        }
+
+        public void GoEnd()
+        {
+            SelectedItem = Content.Length - 1;
+        }
+
+        public static long DirSize(DirectoryInfo dir)
+        {
+            long size = 0;
+            
+            FileInfo[] fis = dir.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            
+            DirectoryInfo[] dis = dir.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
     }
 }
--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -1,12 +1,119 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FileManager
 {
+    enum ViewMode
+    {
+        ShowDirContent,
+        ShowFileContent
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo dir = new DirectoryInfo(@"G:\Games\2term\PP2\Labs");
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(
+                new Layer
+                {
+                    Content = dir.GetFileSystemInfos()
+                }
+            );
+
+            ViewMode viewMode = ViewMode.ShowDirContent;
+            bool esc = false;
+            while (!esc)
+            {
+                if (viewMode == ViewMode.ShowDirContent)
+                {
+                    history.Peek().Draw();
+                }
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        history.Peek().ScrollUp();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        history.Peek().ScrollDown();
+                        break;
+
+                    case ConsoleKey.Enter:
+                        int index = history.Peek().SelectedItem;
+                        FileSystemInfo fsi = history.Peek().Content[index];
+
+                        if (fsi.GetType() == typeof(DirectoryInfo))
+                        {
+                            viewMode = ViewMode.ShowDirContent;
+                            DirectoryInfo selectedDir = fsi as DirectoryInfo;
+                            history.Push(new Layer { Content = selectedDir.GetFileSystemInfos() });
+                        }
+                        else
+                        {
+                            viewMode = ViewMode.ShowFileContent;
+                            using (FileStream fs = new FileStream(fsi.FullName, FileMode.Open, FileAccess.Read))
+                            {
+                                using (StreamReader sr = new StreamReader(fs))
+                                {
+                                    Console.BackgroundColor = ConsoleColor.White;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
+                        }
+                        break;
+
+                    case ConsoleKey.Delete:
+                        int index2 = history.Peek().SelectedItem;
+                        FileSystemInfo fsi2 = history.Peek().Content[index2];
+                        history.Peek().SelectedItem--;
+
+                        if (fsi2.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo directoryInfo = fsi2 as DirectoryInfo;
+                            Directory.Delete(fsi2.FullName, true);
+                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos();
+                        }
+                        else
+                        {
+                            FileInfo fileInfo = fsi2 as FileInfo;
+                            File.Delete(fsi2.FullName);
+                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos();
+                        }
+                        break;
+
+                    case ConsoleKey.Backspace:
+                        if (viewMode == ViewMode.ShowDirContent)
+                        {
+                            history.Pop();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.White;
+                            viewMode = ViewMode.ShowDirContent;
+                        }
+                        break;
+
+                    case ConsoleKey.Home:
+                        history.Peek().GoBegin();
+                        break;
+
+                    case ConsoleKey.End:
+                        history.Peek().GoEnd();
+                        break;
+
+                    case ConsoleKey.Escape:
+                        esc = true;
+                        break;
+                }
+            }
         }
     }
 }
